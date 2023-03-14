e95c2aebdb9ca5222248ef5a03ca2f281e8ee5f7
Prabhatlodhi <lodhi.prabhat@gmail.com>
2023-03-14 06:02:47 +0530

    aa

--- src/App.js
+++ src/App.js
@@ -4,17 +4,39 @@ import "../index.css"
 import HeaderComponent from "./components/HeaderComponent";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/NavComponent/About";
+import ContactUs from "./components/NavComponent/ContactUs";
+
 
 const AppLayout = ()=>{
     return (
-       <>
+        <>
          <HeaderComponent/>
         <Body/>
         <Footer/>
        </>
     )
 }
+//ALways right below so that Applayout load first and then the ROUTER
+const appRouter = createBrowserRouter([
+   {
+    path:"/",
+    element: <AppLayout/>
+   },
+   
+   {
+    path:"/about",
+    element: <About/>
+   },
+   
+   {
+    path:"/contactus",
+    element: <ContactUs/>
+   },
+])
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<AppLayout/>
+root.render(<RouterProvider router={appRouter}/>
  )
---
+++ src/components/NavComponent/ContactUs.js
@@ -0,0 +1,10 @@
+
+
+const ContactUs = ()=>{
+    return(
+        <div>
+            <h1>Hello From About Us</h1>
+        </div>
+    )
+}
+export default ContactUs;
